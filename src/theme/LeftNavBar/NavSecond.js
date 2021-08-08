8fcbe68da5af71daca3afa30aef35640b92f2fdf
louiscklaw <louiscklaw@gmail.com>
2021-08-09 01:21:22 +0800

    update,

--- src/theme/LeftNavBar/NavSecond.js
+++ src/theme/LeftNavBar/NavSecond.js
@@ -2,32 +2,42 @@ import React from "react";
 import Link from "@docusaurus/Link";
 
 import { THEME_WHITE } from "../styles/colors";
-import { Box } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 
 export default function NavSecond() {
   return (
     <>
       <Box style={{ height: "34vh" }}>
-        <div>
-          <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
-            PROJECTS
-          </Link>
-        </div>
-        <div>
-          <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
-            ABOUT
-          </Link>{" "}
-        </div>
-        <div>
-          <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
-            CREDITS
-          </Link>{" "}
-        </div>
-        <div>
-          <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
-            TRAVIS BUILD DASHBOARD
-          </Link>
-        </div>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ height: "100%" }}
+        >
+          <Box mt={2}>
+            <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
+              PROJECTS
+            </Link>
+          </Box>
+
+          <Box mt={2}>
+            <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
+              ABOUT
+            </Link>{" "}
+          </Box>
+          <Box mt={2}>
+            <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
+              CREDITS
+            </Link>{" "}
+          </Box>
+          <Box mt={2}>
+            <Link isNavLink to={"/"} style={{ color: THEME_WHITE }}>
+              TRAVIS BUILD DASHBOARD
+            </Link>
+          </Box>
+        </Grid>
       </Box>
     </>
   );

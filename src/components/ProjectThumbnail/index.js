76d5109eb7cd8180be3fe01815769d3eda1a4f42
louiscklaw <louiscklaw@gmail.com>
2021-08-09 13:22:48 +0800

    update,

--- src/components/ProjectThumbnail/index.js
+++ src/components/ProjectThumbnail/index.js
@@ -70,12 +70,7 @@ export default function ProjectThumbnail({ doc }) {
             </>
           ) : (
             <>
-              <div
-                style={{
-                  width: "100%",
-                  height: "100%",
-                }}
-              ></div>
+              <div style={{ width: "100%", height: "100%" }}></div>
             </>
           )}
         </Link>

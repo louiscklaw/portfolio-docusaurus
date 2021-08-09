4f2a8c9a01dbe5a41c4b9f4d00975c2c908a4f3d
louiscklaw <louiscklaw@gmail.com>
2021-08-09 13:22:41 +0800

    update,

--- src/components/ProjectThumbnail/index.js
+++ src/components/ProjectThumbnail/index.js
@@ -1,21 +1,83 @@
 import React from "react";
 import Link from "@docusaurus/Link";
+import { Box, Grid } from "@material-ui/core";
 
-import { index_thumbnails } from "../../theme/index_thumbnails";
+import { project_thumbnails } from "../../theme/project_thumbnails";
+import { FiArrowRightCircle } from "react-icons/fi";
+import { THEME_WHITE } from "../../theme/styles/colors";
 
 export default function ProjectThumbnail({ doc }) {
+  let [project_thumbnail_blur, setProjectThumbnailBlur] = React.useState(false);
+  let title = project_thumbnails[doc.id] && project_thumbnails[doc.id].title;
   return (
     <>
       <div
+        onMouseEnter={(e) => setProjectThumbnailBlur(true)}
+        onMouseLeave={(e) => setProjectThumbnailBlur(false)}
         style={{
           height: "100%",
           backgroundSize: "cover",
           backgroundPosition: "center",
-          backgroundImage: `url(${index_thumbnails[doc.id]})`,
+          backgroundImage: `url(${
+            project_thumbnails[doc.id] && project_thumbnails[doc.id].thumbnail
+          })`,
         }}
       >
         <Link isNavLink to={doc.path}>
-          {doc.id}
+          {project_thumbnail_blur ? (
+            <>
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  backdropFilter: "blur(10px)",
+                }}
+              >
+                <Grid
+                  container
+                  flexFlow="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                  }}
+                >
+                  <Grid
+                    container
+                    item
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Grid item>
+                      <Box
+                        style={{
+                          color: THEME_WHITE,
+                        }}
+                      >
+                        {title}
+                      </Box>
+                      <Box
+                        mt={"1rem"}
+                        style={{ color: THEME_WHITE, textAlign: "center" }}
+                      >
+                        <FiArrowRightCircle size={"2rem"} />
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </div>
+            </>
+          ) : (
+            <>
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              ></div>
+            </>
+          )}
         </Link>
       </div>
     </>
---
+++ src/theme/project_thumbnails.js
@@ -0,0 +1,71 @@
+import allwinner_v3s_tryout_thumbnail from "../../docs/assets/allwinner_v3s_tryout/thumbnail.jpg";
+import tradingview_dashboard_thumbnail from "../../docs/assets/tradingview_dashboard/thumbnail.jpg";
+import appium_behave_notes_thumbnail from "../../docs/assets/appium_behave_notes/thumbnail.jpg";
+import daily_job_search_thumbnail from "../../docs/assets/daily_job_search/thumbnail.jpg";
+import hk_data_searcher_thumbnail from "../../docs/assets/hk_data_searcher/thumbnail.jpg";
+import hko_weather_bot_thumbnail from "../../docs/assets/hko_weather_bot/thumbnail.jpg";
+import m5stack_playlist_thumbnail from "../../docs/assets/m5stack_playlist/thumbnail.jpg";
+import my_linux_toolbox_thumbnail from "../../docs/assets/my_linux_toolbox/thumbnail.jpg";
+import opencv_car_counting_tryout_thumbnail from "../../docs/assets/opencv_car_counting_tryout/thumbnail.jpg";
+import pet_projects_thumbnail from "../../docs/assets/pet_projects/thumbnail.jpg";
+import portfolio_tryout_thumbnail from "../../docs/assets/portfolio_tryout/thumbnail.jpg";
+import printer_board_tryout_thumbnail from "../../docs/assets/printer_board_tryout/thumbnail.jpg";
+import rthk31_rthk32_linux_viewer_thumbnail from "../../docs/assets/rthk31_rthk32_linux_viewer/thumbnail.jpg";
+import slic3r_tryout_thumbnail from "../../docs/assets/slic3r_tryout/thumbnail.jpg";
+import tappy_tryout_thumbnail from "../../docs/assets/tappy_tryout/thumbnail.jpg";
+
+const project_thumbnails = {
+  allwinner_v3s_tryout: {
+    title: "allwinner_v3s_tryout_project_title",
+    thumbnail: allwinner_v3s_tryout_thumbnail,
+  },
+  tradingview_dashboard: {
+    title: "project_title",
+    thumbnail: tradingview_dashboard_thumbnail,
+  },
+  appium_behave_notes: {
+    title: "project_title",
+    thumbnail: appium_behave_notes_thumbnail,
+  },
+  daily_job_search: {
+    title: "project_title",
+    thumbnail: daily_job_search_thumbnail,
+  },
+  hk_data_searcher: {
+    title: "project_title",
+    thumbnail: hk_data_searcher_thumbnail,
+  },
+  hko_weather_bot: {
+    title: "project_title",
+    thumbnail: hko_weather_bot_thumbnail,
+  },
+  m5stack_playlist: {
+    title: "project_title",
+    thumbnail: m5stack_playlist_thumbnail,
+  },
+  my_linux_toolbox: {
+    title: "project_title",
+    thumbnail: my_linux_toolbox_thumbnail,
+  },
+  opencv_car_counting_tryout: {
+    title: "project_title",
+    thumbnail: opencv_car_counting_tryout_thumbnail,
+  },
+  pet_projects: { title: "project_title", thumbnail: pet_projects_thumbnail },
+  portfolio_tryout: {
+    title: "project_title",
+    thumbnail: portfolio_tryout_thumbnail,
+  },
+  printer_board_tryout: {
+    title: "project_title",
+    thumbnail: printer_board_tryout_thumbnail,
+  },
+  rthk31_rthk32_linux_viewer: {
+    title: "project_title",
+    thumbnail: rthk31_rthk32_linux_viewer_thumbnail,
+  },
+  slic3r_tryout: { title: "project_title", thumbnail: slic3r_tryout_thumbnail },
+  tappy_tryout: { title: "project_title", thumbnail: tappy_tryout_thumbnail },
+};
+
+export { project_thumbnails };
